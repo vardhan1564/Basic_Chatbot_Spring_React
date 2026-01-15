3030c5a47583c35a96854052b7c43c22f7f1b8ba
vardhan1564 <vardhanadheli1527@gmail.com>
2026-01-15 22:17:15 +0530

    frontend changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api';
+
+const API_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api';
 
 // Create an Axios instance
 const api = axios.create({
